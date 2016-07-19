8e45728545225aebe3a575baa02a50fcb6df6d9d
Kay <kayvanderlans@gmail.com>
2016-07-19 11:58:28 +0200

    Beginning of the GameController

---
+++ TheNews/Assets/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Handles the relationships between the player and the different entities in the game,
+/// This gameobject will never get destroyed.
+/// </summary>
+public class GameController : MonoBehaviour
+{
+    public Dictionary<string, int> relationships = new Dictionary<string, int>();
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        relationships.Add("boss", 50);
+        relationships.Add("dictator", 50);
+        relationships.Add("citizens", 50);
+    }
+
+    void Update()
+    {
+        //Handle choices
+        // if (choosesForCitizens) { relationships["boss"] -= 5; }
+    }
+}
