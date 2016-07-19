793c82e74190859090577ef9cce19c45c9458758
Ricardo Desmedt <Ricardo-42-ajax@live.nl>
2016-07-19 12:00:05 +0200

    Camera + Mouse beginning

---
+++ TheNews/Assets/Scripts123/MouseInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseInput : MonoBehaviour {
+
+	private Vector3 mousePosition = Input.mousePosition;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
